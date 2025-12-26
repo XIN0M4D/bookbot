1629fe94b5ac901e3901f80ec08e003491fb0f6a
XIN0M4D <j_nunns@hotmail.co.uk>
2025-12-26 07:41:19 +0000

    D: updated stats,py with new function to count characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import num_words
+from stats import num_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,8 +8,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count_words = num_words(text)
+    char_list = count_characters(text)
     print(f"Found {count_words} total words")
-
+    print(f"found the following characters {char_list}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def num_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    unique_characters = {}
+    for ch in text:
+        lowered = ch.lower()
+        if lowered in unique_characters:
+            unique_characters[lowered] += 1
+        else:
+            unique_characters[lowered] = 1
+    return unique_characters
